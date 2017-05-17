6a85de88316012545bdc330db4a68c8b2ccdb3db
EAKHawkEye <marino.mike@gmail.com>
2017-05-16 21:21:22 -0700

    features added: cmd opt, scp opt

--- MultiSCP/multiscp.sh
+++ MultiSCP/multiscp.sh
@@ -10,8 +10,8 @@
 #############
 max_conn=100
 timeout_conn=10
-my_file=
-my_host_list=
+data_file=
+host_file=
 
 
 
@@ -21,32 +21,33 @@ my_host_list=
 function transfer_files()
 {
 	# Transfer the files to the brokers
-	local ary_process=()
-	unset ary_process
 	local my_file=${1}
 	local host_list=${2}
 	local max_connections=${3}
 	local timeout_conn=${4}
+	local scp_options=${5}
 	local count=0
+	local ary_process=()
 	local max_hosts=$(cat ${host_list} | wc -l)
 
+	# Iterate through all the hostnames in the file
 	for my_host in $(cat ${host_list}); do 
 
 		# Copy the files and add the PID to the array
-		scp -o ConnectTimeout=${timeout_conn} -q ${my_file} ${my_host}:. &
+		scp ${scp_options} -o ConnectTimeout=${timeout_conn} -q ${my_file} ${my_host}:. &
 		ary_process+=( $! )
 		let "count++"
 
 		# Wait on the processes (pids) to complete then do next batch
 		if [[ ${#ary_process[@]} -ge ${max_connections} || ${count} -ge ${max_hosts} ]]; then
 			for my_pid in ${ary_process[@]}; do
-				echo -en "   Waiting on copies: ${count} left    \r"
+				echo -en "    Waiting on copies: ${count} left    \r"
 				wait ${my_pid};
 				let "count--"
 			done
 			unset ary_process
 			count=0
-			echo "   Waiting on copies: complete     "
+			echo "    Waiting on copies: complete     "
 		fi
 
 	done
@@ -54,9 +55,18 @@ function transfer_files()
 	return 0
 }
 
-function help_me()
+function usage()
 {
-	echo "  Usage: $0 <file to transfer> <hosts file>"
+	# [USER OUTPUT] Help
+	echo -e "  Usage: $( basename $0 ) action -h <target_hosts_file> -f <file_to_transfer> [options...]"
+	echo -e "\n      Arguments:"
+	echo -e "\t        -h         file of hostnames (1/line) | -h hosts/impacted.lst"
+	echo -e "\t        -f         file to transfer           | -f auto-fix.sh"
+	echo -e "        Options:"
+	echo -e "\t        -m         max concurrent connections | -m 100"
+	echo -e "\t        -t         connetion timeout (in sec) | -t 10"
+	echo -e "\t        -o         ssh/scp options            | -o \"-P 44321 -o StrictHostKeyChecking=no\"\n"
+
 	exit 1
 }
 
@@ -66,16 +76,28 @@ function help_me()
 # Logic #
 #########
 # Pass a file of hostnames to the script
-if ! [[ $# -eq 2 ]]; then help_me; fi
+if [[ $# -lt 4 ]]; then usage; fi
 
-my_file=${1}
-my_host_list=${2}
+# [INPUT PARSE] - Arguments
+while [ "${1}" ]; do
+	case "${1}" in
+		"-h" | "--host"* ) host_file=${2}; shift ;;
+		"-f" | "--file"* ) data_file=${2}; shift ;;
+		"-m" | "--max"*  ) max_conn=${2}; shift ;;
+		"-t" | "--time"* ) timeout_conn=${2}; shift ;;
+		"-o" | "--ssho"* ) ssh_options=${2}; shift ;;
+					   * ) echo "Option: '${1}' is unsupported"; usage;;
+	esac
+	shift
+done
 
 # Make sure the hostlist exists
-if ! [ -e ${my_host_list} ]; then echo "  Missing ${my_host_list}"; fi
+if ! [ -e ${host_file} ]; then echo "  Missing ${host_file}"; exit 3; fi
+if ! [ -e ${data_file} ]; then echo "  Missing ${host_file}"; exit 3; fi
 
 # Process the hostlist
-echo "  Transfering ${my_file} to ${my_host_list} hosts;"
-transfer_files "${my_file}" "${my_host_list}" ${max_conn} ${timeout_conn}
+echo "  Transferring '${data_file}' to hosts in ${host_file}"
+transfer_files "${data_file}" "${host_file}" ${max_conn} ${timeout_conn} "${ssh_options}"
+
 
 exit 0
--- MultiSCP/readme.md
+++ MultiSCP/readme.md
@@ -6,15 +6,25 @@ A native bash tool to scp in parellel.
 Usage
 -------------
 Simply pass two files: the file you want to transfer and a list of hostnames (one per line). The parallelism is hardcoded at 100 right now. I'll add an argument for this later.
+Note: Intended use with SSH Keys already setup.
 
 ```
-  Usage: multiscp.sh <file to transfer> <hosts file>
+  Usage: multiscp.sh action -h <target_hosts_file> -f <file_to_transfer> [options...]
+
+      Arguments:
+	        -h         file of hostnames (1/line) | -h hosts/impacted.lst
+	        -f         file to transfer           | -f auto-fix.sh
+        Options:
+	        -m         max concurrent connections | -m 100
+	        -t         connetion timeout (in sec) | -t 10
+	        -o         ssh/scp options            | -o "-P 44321 -o StrictHostKeyChecking=no"
+
 ```
 
 Example
 -------------
 ```
--$ multiscp.sh auto-fix.sh hosts/impacted-hosts.lst
-  Transfering auto-fix.sh to hosts/impacted-hosts.lst hosts;
-   Waiting on copies: complete
+-$ multiscp.sh -f auto-fix.sh -h hosts/impacted-hosts.lst
+  Transferring 'auto-fix.sh' to hosts in hosts/impacted-hosts.lst
+    Waiting on copies: complete
 ```
